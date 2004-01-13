2871d62c724ee803ea539c397a6f643e2662bb2f
menno <menno>
2004-01-13 14:24:10 +0000

    moved reading of SBR data for SBR to a better place (DRM may be broken again) changed filterbank overlap data saving so that it uses seperate data If this works SBR processing can be moved to channel reconstruction

--- libfaad/sbr_syntax.c
+++ libfaad/sbr_syntax.c
@@ -22,7 +22,7 @@
 ** Commercial non-GPL licensing of this software is possible.
 ** For more info contact Ahead Software through Mpeg4AAClicense@nero.com.
 **
-** $Id: sbr_syntax.c,v 1.20 2004/01/12 18:55:40 menno Exp $
+** $Id: sbr_syntax.c,v 1.21 2004/01/13 14:24:10 menno Exp $
 **/
 
 #include "common.h"
